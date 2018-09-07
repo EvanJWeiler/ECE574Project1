4d0312756af021a0d81d70f5ccaf2f1a8c48ded4
Lknott600 <39837019+Lknott600@users.noreply.github.com>
2018-09-07 10:56:47 -0700

    circuit1 addition

---
+++ ECE574AProject1.srcs/sources_1/new/circuit1.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/06/2018 02:19:59 PM
+// Design Name: 
+// Module Name: circuit1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module circuit1(a, b, c, z, x);
+    input [7:0] a, b, c;
+    output reg [7:0] z; 
+    output reg [15:0] x;
+    wire [7:0] d, e;
+    wire [15:0] f, g, xWire;
+    
+    ADD add1(a, b, d);
+    ADD add2(a, c, e);
+    COMP comp1(d, e, g);
+    MUX2x1 mux1(g, d, e, z);
+    MUL mul1(a, c, f);
+    SUB sub1(f, d, xWire);
+    REG reg1(x, xWire);
+endmodule
