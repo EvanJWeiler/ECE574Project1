940affc4b8340e762f23b48447634efdbc10295b
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-09-14 14:25:24 -0700

    Re-wrote due to circuit errors

--- ECE574AProject1.srcs/sources_1/new/Circuit 3.v
+++ ECE574AProject1.srcs/sources_1/new/Circuit 3.v
@@ -20,24 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Circuit3(a, b, c, d, e, f, g, h, sa, avg);
-input [15:0] a, b, c, d, e, f, g, h;
-input [7:0] sa;
-output [15:0] avg;
-
-wire [31:0] l00, l01, l02, l03, l10, l11, l2, l2div2, l2div4, l2div8;
-
-ADD add1(a, b, l00);
-ADD add2(c, d, l01);
-ADD add3(e, f, l02);
-ADD add4(g, h, l03);
-ADD add5(l00, l01, l10);
-ADD add6(l02, l03, l11);
-ADD add7(l10, l11, l2);
-SHR shr1(l2, sa, l2div2);
-SHR shr2(l2div2, sa, l2div4);
-SHR shr3(l2div4, sa, l2div8);
-REG reg1(avg,l2div8);
+module Circuit3(a, b, c, d, e, f, g, h, sa, avg, Clk, Rst);
+    input [15:0] a, b, c, d, e, f, g, h;
+    input [7:0] sa;
+    input Clk, Rst;
+    output [15:0] avg;
+    
+    wire [31:0] l00, l01, l02, l03, l10, l11, l2, l2div2, l2div4, l2div8;
+    
+    ADD add1(a, b, l00);
+    ADD add2(c, d, l01);
+    ADD add3(e, f, l02);
+    ADD add4(g, h, l03);
+    ADD add5(l00, l01, l10);
+    ADD add6(l02, l03, l11);
+    ADD add7(l10, l11, l2);
+    SHR shr1(l2, sa, l2div2);
+    SHR shr2(l2div2, sa, l2div4);
+    SHR shr3(l2div4, sa, l2div8);
+    REG reg1(l2div8, Clk, Rst, avg);
 
 //l00 = a + b
 //l01 = c + d
