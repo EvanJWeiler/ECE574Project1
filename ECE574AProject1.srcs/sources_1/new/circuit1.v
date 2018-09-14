5d2a2ecea8ca5c79f32edb46a11c50ec53b51fd2
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-09-14 10:38:02 -0700

    Added gEQ, gLT, gGT

--- ECE574AProject1.srcs/sources_1/new/circuit1.v
+++ ECE574AProject1.srcs/sources_1/new/circuit1.v
@@ -26,10 +26,11 @@ module circuit1(a, b, c, z, x);
     output reg [15:0] x;
     wire [7:0] d, e;
     wire [15:0] f, g, xWire;
+    wire g, l, eq;
     
     ADD add1(a, b, d);
     ADD add2(a, c, e);
-    COMP comp1(d, e, g);
+    COMP comp1(d, e, g, l, eq);
     MUX2x1 mux1(g, d, e, z);
     MUL mul1(a, c, f);
     SUB sub1(f, d, xWire);
